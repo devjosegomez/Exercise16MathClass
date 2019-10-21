9705387c35394ca476233c8651ddd4c8025be666
devjosegomez <devjosegomez@gmail.com>
2019-10-21 13:14:57 -0500

    Initial commit

---
+++ src/com/exercise16mathclass/app/MathematicalOperations.java
@@ -0,0 +1,17 @@
+package com.exercise16mathclass.app;
+import java.lang.Math;
+public class MathematicalOperations 
+{
+	public static void main(String [] args) 
+	{
+		double number = 3.4;
+		System.out.println("e value: " + Math.E);
+		System.out.println("PI value: " + Math.PI);
+		System.out.println("Absolute value of -2.15 is " + Math.abs(-2.15));
+		System.out.println("The cube root of 27 is " + Math.cbrt(27));
+		System.out.println("The round of 3.76 is " + Math.round(number));
+		System.out.println("The ceil of 3.76 is " + Math.ceil(3.76));
+		System.out.println("The floor of 3.76 is " + Math.floor(number));
+		System.out.println();
+	}
+}
